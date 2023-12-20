9cf648df86d3a2bb2de4e005eb531fdbc98ef830
ThanhVo15 <thanhvm21416c@st.uel.edu.vn>
2023-12-21 03:17:54 +0700

    Học lần 1:
     Video 4, 2 phút hơn

---
+++ Week 1.py
@@ -0,0 +1,19 @@
+'''
+1. First Lesson (2:30 a.m. 21/12/20230)
+1.1 Các kiểu dữ liệu cơ bản trong Python
+- Int: Kiểu số nguyên ( Không có chứa dấu chấm thập phân), có thể là các số dương hoặc âm
+Ví dụ: 113, -114
+- float: Kiểu số thực ( có chứa dấu chấm thập phân), có thể là số dương hoặc âm
+Ví dụ: 5.2, 7,3
+- Kiểu complex: Kiểu số phức,
+    - Vd 1: z = 2+3j thì 2 là phần thực, 3 là phần ảo (j là từ khóa để đánh dấu phần ảo)
+    - Vd 2: z = complex(2,3) thì 2 là phần thực, 3 là phần ảo
+    - Khi xuất kết quả ta có thể xuất ra bằng các cú pháp:
+        - print("Phần thực:",z.real) ==> Phần thực: 2
+        - print("Phần ảo:", z.imag) ==> Phần ảo: 3
+- Kiểu str: Kiểu chuỗi, để trong nháy đôi hoặc nháy đơn
+Ví dụ: "Obama", "Thành Đẹp Trai"
+- Kiểu bool: Kiểu luận lý, để lưu True hoặc False
+Ví dụ: t1: True
+        t2: False
+'''
